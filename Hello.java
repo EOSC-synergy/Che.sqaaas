4da8716906f1f4c5c2af8cbcd6126a5da9711cd4
Muhamamd Yousaf <141431746+Yousaf95@users.noreply.github.com>
2024-05-20 13:24:58 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
-public class Hello (
-public static void main(String[] args)
-  {
-    //Prints "hell0, world" in the terminal window.
-    System.out.println("Hello, Horld");
-  }
+public class Hello {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
 }

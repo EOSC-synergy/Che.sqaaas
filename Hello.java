3b464d864bf338785985829e226cb21feb55efae
Muhamamd Yousaf <141431746+Yousaf95@users.noreply.github.com>
2024-05-20 12:40:30 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-publlc class Mello (
+public class Hello (
 public static void main(String[] args)
   {
-    //Prints "hell0,horld" in the terminal windom.
+    //Prints "hell0, world" in the terminal window.
     System.out.println("Hello, Horld");
   }
 }
